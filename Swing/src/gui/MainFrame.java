00d87377b375664ab3226ff0f4950cac810f96ba
benja <benja@DESKTOP-AP62BTH>
2016-11-14 20:06:49 -0500

    Moved classes to gui and created model package

--- Swing/src/gui/MainFrame.java
+++ Swing/src/gui/MainFrame.java
@@ -1,9 +1,11 @@
+package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -17,6 +19,7 @@ public class MainFrame extends JFrame {
 	private Toolbar toolbar;
 	private TextPanel textPanel;
 	private FormPanel formPanel;
+	private JFileChooser fileChooser;
 
 	public MainFrame() {
 		super("Hello World");
@@ -26,6 +29,9 @@ public class MainFrame extends JFrame {
 		toolbar = new Toolbar();
 		textPanel = new TextPanel();
 		formPanel = new FormPanel();
+		fileChooser = new JFileChooser();
+		
+		fileChooser.addChoosableFileFilter(new PersonFileFilter());
 
 		setJMenuBar(createMenuBar());
 
@@ -85,9 +91,20 @@ public class MainFrame extends JFrame {
 		exitItem.setMnemonic(KeyEvent.VK_X);
 
 		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+		
+		importDataItem.addActionListener(a -> {
+			if(fileChooser.showOpenDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION) {
+				System.out.println(fileChooser.getSelectedFile());
+			}
+		});
+		
+		exportDataItem.addActionListener(a -> {
+			if(fileChooser.showSaveDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION) {
+				System.out.println(fileChooser.getSelectedFile());
+			}
+		});
 
 		exitItem.addActionListener(a -> {
-			
 			int action = JOptionPane.showConfirmDialog(MainFrame.this, "Do you really want to exit the application?", "Confirm Exit",
 					JOptionPane.OK_CANCEL_OPTION);
 			if(action == JOptionPane.OK_OPTION) {
--- Swing/src/gui/Toolbar.java
+++ Swing/src/gui/Toolbar.java
@@ -1,3 +1,4 @@
+package gui;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
